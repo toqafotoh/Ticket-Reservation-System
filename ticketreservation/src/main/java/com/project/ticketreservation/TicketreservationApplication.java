8545e6ec6cd27b28a8e78c8d92aaecc19f13fc44
Hazem Ahmed <111501299+Hazem-Ahmed1@users.noreply.github.com>
2024-04-17 21:14:33 +0200

    yala benaaaa

---
+++ ticketreservation/src/main/java/com/project/ticketreservation/TicketreservationApplication.java
@@ -0,0 +1,13 @@
+package com.project.ticketreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketreservationApplication.class, args);
+	}
+
+}
---
+++ ticketreservation/src/test/java/com/project/ticketreservation/TicketreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ticketreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
