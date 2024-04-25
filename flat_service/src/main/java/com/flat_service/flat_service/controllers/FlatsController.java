031d8250296eb5857c684f205747cab1b533ebf3
Hazem Ahmed <111501299+Hazem-Ahmed1@users.noreply.github.com>
2024-04-26 02:55:15 +0300

    testing Fragments & new service

---
+++ flat_service/src/main/java/com/flat_service/flat_service/controllers/FlatsController.java
@@ -0,0 +1,12 @@
+package com.flat_service.flat_service.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class FlatsController {
+    
+
+    
+}
