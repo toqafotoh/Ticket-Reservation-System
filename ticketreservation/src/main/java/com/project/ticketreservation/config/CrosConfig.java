d70f3511a140ab6c581746769ae3d815e35ecebc
gimmeursocks <bobteen1@gmail.com>
2024-05-06 18:04:06 +0300

    aop done

--- ticketreservation/src/main/java/com/project/ticketreservation/config/CrosConfig.java
+++ ticketreservation/src/main/java/com/project/ticketreservation/config/CrosConfig.java
@@ -1,11 +1,13 @@
 package com.project.ticketreservation.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableAspectJAutoProxy()
 public class CrosConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
